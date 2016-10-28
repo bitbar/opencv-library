c73add829f488c74ac5f30b0cd9878fa566cd59a
Severi Haverila <severi.haverila@bitbar.com>
2016-10-28 15:28:45 -0700

    general refactoring

--- src/main/java/library/ImageRecognition.java
+++ src/main/java/library/ImageRecognition.java
@@ -27,68 +27,81 @@ public class ImageRecognition {
     //The "scene" parameter is the image in which we are looking for "image"
     // "tolerance" sets the required accuracy for the image recognition algorithm.
     public static Point[] findImage(String image, String scene, ImageRecognitionSettingsDTO settings, String platformName, Dimension screenSize) throws Exception {
-        Point[] imgRect = new Point[0];
-        Point[] imgRectScaled;
-
         log("Searching for " + image);
         log("Searching in " + scene);
-        try {
+        Point[] imgRect = findImageUsingAkaze(image, scene, settings);
+
+        if (imgRect != null) {
+            if (platformName.equalsIgnoreCase("iOS")) {
+                imgRect = scaleImageRectangleForIos(screenSize, imgRect);
+            }
+            Point center = imgRect[4];
+            if (!isPointInsideScreenBounds(center, screenSize)) {
+                log("Screen size is (width, height): " + screenSize.getWidth() + ", " + screenSize.getHeight());
+                log("WARNING: Coordinates found do not match the screen --> image not found.");
+                imgRect = null;
+            }
+        }
+        return imgRect;
+    }
+
+
+
+	private static Point[] findImageUsingAkaze(String image, String scene, ImageRecognitionSettingsDTO settings) {
+		Point[] imgRect = new Point[0];
+		try {
             imgRect = imageFinder.findImage(image, scene, settings.getTolerance());
         } catch (Exception e) {
             e.printStackTrace();
         }
+		return imgRect;
+	}
 
-        if (imgRect != null) {
 
-            if (platformName.equalsIgnoreCase("iOS")) {
-                //for retina devices we need to recalculate coordinates
-                double sceneHeight = imageFinder.getSceneHeight();
-                double sceneWidth = imageFinder.getSceneWidth();
 
-                int screenHeight = screenSize.getHeight();
-                int screenWidth = screenSize.getWidth();
+	private static Point[] scaleImageRectangleForIos(Dimension screenSize, Point[] imgRect) {
+		Point[] imgRectScaled;
+		//for retina devices we need to recalculate coordinates
+		double sceneHeight = imageFinder.getSceneHeight();
+		double sceneWidth = imageFinder.getSceneWidth();
 
-                // Make sure screenshot size values are "landscape" for comparison
-                if (sceneHeight > sceneWidth) {
-                    double temp = sceneHeight;
-                    sceneHeight = sceneWidth;
-                    sceneWidth = temp;
-                }
+		int screenHeight = screenSize.getHeight();
+		int screenWidth = screenSize.getWidth();
 
-                // Make sure screen size values are "landscape" for comparison
-                if (screenHeight > screenWidth) {
-                    int temp = screenHeight;
-                    screenHeight = screenWidth;
-                    screenWidth = temp;
-                }
+		// Make sure screenshot size values are "landscape" for comparison
+		if (sceneHeight > sceneWidth) {
+		    double temp = sceneHeight;
+		    sceneHeight = sceneWidth;
+		    sceneWidth = temp;
+		}
 
-                if ((screenHeight<sceneHeight) && (screenWidth<sceneWidth)) {
-                    if ((screenHeight<sceneHeight/2)&&(screenWidth<sceneWidth/2)) {
-                        imgRectScaled = new Point[]{new Point(imgRect[0].x / 3, imgRect[0].y / 3), new Point(imgRect[1].x / 3, imgRect[1].y / 3), new Point(imgRect[2].x / 3, imgRect[2].y / 3), new Point(imgRect[3].x / 3, imgRect[3].y / 3), new Point(imgRect[4].x / 3, imgRect[4].y / 3)};
-                        log("Device with Retina display rendered at x3 => coordinates have been recalculated");
-                        imgRect = imgRectScaled;
-                    }
-                    else {
-                        imgRectScaled = new Point[]{new Point(imgRect[0].x / 2, imgRect[0].y / 2), new Point(imgRect[1].x / 2, imgRect[1].y / 2), new Point(imgRect[2].x / 2, imgRect[2].y / 2), new Point(imgRect[3].x / 2, imgRect[3].y / 2), new Point(imgRect[4].x / 2, imgRect[4].y / 2)};
-                        log("Device with Retina display rendered at x2 => coordinates have been recalculated");
-                        imgRect = imgRectScaled;
-                    }
-                }
-            }
+		// Make sure screen size values are "landscape" for comparison
+		if (screenHeight > screenWidth) {
+		    int temp = screenHeight;
+		    screenHeight = screenWidth;
+		    screenWidth = temp;
+		}
 
-            Point center = imgRect[4];
+		if ((screenHeight<sceneHeight) && (screenWidth<sceneWidth)) {
+		    if ((screenHeight<sceneHeight/2)&&(screenWidth<sceneWidth/2)) {
+		        imgRectScaled = new Point[]{new Point(imgRect[0].x / 3, imgRect[0].y / 3), new Point(imgRect[1].x / 3, imgRect[1].y / 3), new Point(imgRect[2].x / 3, imgRect[2].y / 3), new Point(imgRect[3].x / 3, imgRect[3].y / 3), new Point(imgRect[4].x / 3, imgRect[4].y / 3)};
+		        log("Device with Retina display rendered at x3 => coordinates have been recalculated");
+		        imgRect = imgRectScaled;
+		    }
+		    else {
+		        imgRectScaled = new Point[]{new Point(imgRect[0].x / 2, imgRect[0].y / 2), new Point(imgRect[1].x / 2, imgRect[1].y / 2), new Point(imgRect[2].x / 2, imgRect[2].y / 2), new Point(imgRect[3].x / 2, imgRect[3].y / 2), new Point(imgRect[4].x / 2, imgRect[4].y / 2)};
+		        log("Device with Retina display rendered at x2 => coordinates have been recalculated");
+		        imgRect = imgRectScaled;
+		    }
+		}
+		return imgRect;
+	}
 
-            // Check that found center coordinate isn't out of screen bounds
-            if ((center.x >= screenSize.width) || (center.x < 0) || (center.y >= screenSize.height) || (center.y < 0)) {
-                log("Screen size is (width, height): " + screenSize.getWidth() + ", " + screenSize.getHeight());
-                log("WARNING: Coordinates found do not match the screen --> image not found.");
-                imgRect = null;
-            } else {
-              return imgRect;
-            }
-        }
-	      return null;
-    }
+
+
+	private static boolean isPointInsideScreenBounds(Point center, Dimension screenSize) {
+		return !((center.x >= screenSize.width) || (center.x < 0) || (center.y >= screenSize.height) || (center.y < 0));
+	}
     
     
     public static String getTextStringFromImage(String imageInput) {
